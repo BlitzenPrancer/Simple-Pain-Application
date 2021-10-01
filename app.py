a6f5dea2a20e0d706d02d087933f43c07641d463
BlitzenPrancer <blitzenprancer666@gmail.com>
2021-10-01 22:16:57 +0530

    added width

---
+++ app.py
@@ -0,0 +1,10 @@
+from tkinter import *
+import PIL
+from PIL import Image, ImageDraw
+
+# defining some constants first
+
+# width of the canvas
+WIDTH, HEIGHT = 500,500
+CENTER = WIDTH // 2
+WHITE = (255, 255, 255)
